9bf5d341017280185975330454c5da418d1d1620
ExcusiveBum <nikodem.wisniewski.nw@gmail.com>
2024-10-15 23:17:02 +0200

    Maybe final commit

--- main.cpp
+++ main.cpp
@@ -7,142 +7,415 @@ enum Month {January = 0, February = 1, March = 2, April = 3, May = 4, June = 5, 
 
 int main()
 {
-    enum Weekday weekday;
-    enum Month month;
-    int d, d0 = 15, fd = 5, m, m0 = 10, fm, y, y0 = 1582, fy;
-    printf("Input date in format dd.mm.yyyy:");
+    int d, d0 = 15, fd = 5, m, m0 = 10, y, y0 = 1582, fy, answer;
+    printf("Input date after %d.%d.%d in format dd.mm.yyyy: ", d0, m0, y0);
     scanf("%d.%d.%d", &d, &m, &y);
-    if (((d>d0 && m>=m0 && y>=y0) || (d<=d0 && m>m0 && y>=y0) || (d<=d0 && m<=m0 && y>y0)) && (d>0 && 0>m>=12 && y>=y0))
+    if (((d>d0 && m>=m0 && y>=y0) || (d<=d0 && m>m0 && y>=y0) || (d<=d0 && m<=m0 && y>y0) || (d>d0 && m<=m0 && y>y0)) && (d>0 && d<=31 && m>0 && m<=12 && y>=y0))
     {
         if (y>y0)
         {
-            if (m < m0)
+            for (int i=m0-1; i<12; i++) // pierwsze miesiące jeśli y>y0
             {
-                for (int i=m0-1; i<12; i++)
+                switch (i)
                 {
-                    switch (i)
+                    case October:
                     {
-                        case October:
-                            fd += 31-d0;
+                        fd += 31-d0;
                         break;
-                        case November:
-                            fd += 30;
+                    }
+                    case November:
+                    {
+                        fd += 30;
                         break;
-                        case December:
-                            fd += 31;
-                        default:
-                            break;
+                    }
+                    case December:
+                    {
+                        fd += 31;
+                        break;
+                    }
+                    default:
+                    {
+                        break;
                     }
                 }
-
-                if (m > 2)
+            }
+            for (int i=y0+1; i<y; i++) // wszystkie miesiące z wszytkich lat oprócz ostatniego
+            {
+                if (i%4==0 && (i%100!=0 || i%400==0))
                 {
-                    for (int i=y0+1; i<=y; i++)
+                    for (int j=0; j<12; j++)
                     {
-                        if (i%4==0 && (i%100!=0 || i%400==0))
+                        switch (j)
                         {
-                            for (int i=0; i<m; i++)
+                            case January:
                             {
-                                switch (i)
-                                {
-                                    case January:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case February:
-                                        fd += i==m-1 ? 29-d : 29;
-                                        break;
-                                    case March:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case April:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case May:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case June:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case July:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case August:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case September:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case October:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case November:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case December:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    default:
-                                        break;
-                                }
+                                fd += 31;
+                                break;
+                            }
+                            case February:
+                            {
+                                fd += 29;
+                                break;
+                            }
+                            case March:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case April:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case May:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case June:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case July:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case August:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case September:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case October:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case November:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case December:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            default:
+                            {
+                                break;
                             }
                         }
-                        else
-                        {
-                            for (int i=0; i<m; i++)
-                            {
-                                switch (i)
-                                {
-                                    case January:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case February:
-                                        fd += i==m-1 ? 28-d : 28;
-                                        break;
-                                    case March:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case April:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case May:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case June:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case July:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case August:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case September:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case October:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    case November:
-                                        fd += i==m-1 ? 30-d : 30;
-                                        break;
-                                    case December:
-                                        fd += i==m-1 ? 31-d : 31;
-                                        break;
-                                    default:
-                                        break;
-                                }
-                            }
-                        }
-
                     }
                 }
                 else
                 {
-
+                    for (int j=0; j<12; j++)
+                    {
+                        switch (j)
+                        {
+                            case January:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case February:
+                            {
+                                fd += 28;
+                                break;
+                            }
+                            case March:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case April:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case May:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case June:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case July:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case August:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case September:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case October:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            case November:
+                            {
+                                fd += 30;
+                                break;
+                            }
+                            case December:
+                            {
+                                fd += 31;
+                                break;
+                            }
+                            default:
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            int i = y; // zliczanie dni dla ostatniego roku
+            if (i%4==0 && (i%100!=0 || i%400==0))
+            {
+                for (int j=0; j<m; j++)
+                {
+                    switch (j)
+                    {
+                        case January:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case February:
+                        {
+                            fd += j==m-1 ? 29-(29-d) : 29;
+                            break;
+                        }
+                        case March:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case April:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case May:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case June:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case July:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case August:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case September:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case October:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case November:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case December:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        default:
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int j=0; j<m; j++)
+                {
+                    switch (j)
+                    {
+                        case January:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case February:
+                        {
+                            fd += j==m-1 ? 28-(28-d) : 28;
+                            break;
+                        }
+                        case March:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case April:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case May:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case June:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case July:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case August:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case September:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case October:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        case November:
+                        {
+                            fd += j==m-1 ? 30-(30-d) : 30;
+                            break;
+                        }
+                        case December:
+                        {
+                            fd += j==m-1 ? 31-(31-d) : 31;
+                            break;
+                        }
+                        default:
+                        {
+                            break;
+                        }
+                    }
                 }
             }
         }
+        else
+        {
+            for (int i=m0-1; i<m; i++)
+            {
+                switch (i)
+                {
+                    case October:
+                    {
+                        fd += i==m-1 ? 31-(31-(d-d0)) : 31-d0;
+                        break;
+                    }
+                    case November:
+                    {
+                        fd += i==m-1 ? 30-(30-d) : 30;
+                        break;
+                    }
+                    case December:
+                    {
+                        fd += i==m-1 ? 31-(31-d) : 31;
+                        break;
+                    }
+                    default:
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        answer = fd%7;
+        switch (answer)
+        {
+            case Monday:
+            {
+                printf("Weekday of %d.%d.%d is Monday", d, m, y);
+                break;
+            }
+            case Tuesday:
+            {
+                printf("Weekday of %d.%d.%d is Tuesday", d, m, y);
+                break;
+            }
+            case Wednesday:
+            {
+                printf("Weekday of %d.%d.%d is Tuesday", d, m, y);
+                break;
+            }
+            case Thursday:
+            {
+                printf("Weekday of %d.%d.%d is Thursday", d, m, y);
+                break;
+            }
+            case Friday:
+            {
+                printf("Weekday of %d.%d.%d is Friday", d, m, y);
+                break;
+            }
+            case Saturday:
+            {
+                printf("Weekday of %d.%d.%d is Saturday", d, m, y);
+                break;
+            }
+            case Sunday:
+            {
+                printf("Weekday of %d.%d.%d is Sunday", d, m, y);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
     }
-
+    else if (d==d0 && m==m0 && y==y0)
+    {
+        printf("Date you have prompted is the same as %d.%d.%d", d0, m0, y0);
+    }
+    else
+    {
+        printf("Date you have prompted is before %d.%d.%d", d0, m0, y0);
+    }
+    printf("\n%d", fd-5);
     return 0;
 }
